a2f04b9c6fc1db9b09467a52595aa8d6d19bd481
smozhevsky <smozhevsky@gmail.com>
2021-02-24 21:22:13 +0300

    add DialogItem and Message components

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,24 +2,32 @@ import React from "react";
 import classes from "./Dialogs.module.css";
 import { NavLink } from "react-router-dom";
 
-const Dialogs = (props) => {
+const DialogItem = (props) => {
+  let path = "/dialogs/" + props.id;
+
+  return (
+    <div className={classes.dialog + " " + classes.active}>
+      <NavLink to={path}>{props.name}</NavLink>
+    </div>
+  );
+};
+
+const Message = (props) => {
+  return <div className={classes.message}>{props.message}</div>;
+};
+
+const Dialogs = () => {
   return (
     <div className={classes.dialogs}>
       <div className={classes.dialogsItems}>
-        <div className={classes.dialog + " " + classes.active}>
-          <NavLink to="/dialogs/1">Ilya</NavLink>
-        </div>
-        <div className={classes.dialog}>
-          <NavLink to="/dialogs/2">Margarita</NavLink>
-        </div>
-        <div className={classes.dialog}>
-          <NavLink to="/dialogs/3">Emily</NavLink>
-        </div>
+        <DialogItem name="Ilya" id="1" />
+        <DialogItem name="Margarita" id="2" />
+        <DialogItem name="Emily" id="3" />
       </div>
       <div className={classes.messages}>
-        <div className={classes.message}>Hi!</div>
-        <div className={classes.message}>How are you?</div>
-        <div className={classes.message}>Yo!</div>
+        <Message message="Hi!" />
+        <Message message="How are you?" />
+        <Message message="Yo!" />
       </div>
     </div>
   );
