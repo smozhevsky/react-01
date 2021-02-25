cf783bd76c77ab823313f4431008b865efa363fe
smozhevsky <smozhevsky@gmail.com>
2021-02-25 14:55:27 +0300

    add postsElements array (.map)

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,10 +3,14 @@ import classes from "./MyPosts.module.css";
 import Post from "./Post/Post.jsx";
 
 const MyPosts = () => {
-  let postData = [
+  let posts = [
     { id: 2, message: "How are you?", likesCount: 5 },
     { id: 3, message: "It is my first post!", likesCount: 11 },
   ];
+
+  let postsElements = posts.map((p) => {
+    return <Post message={p.message} likeCount={p.likesCount} />;
+  });
 
   return (
     <div className={classes.postsBlock}>
@@ -19,16 +23,7 @@ const MyPosts = () => {
           <button>Add new post</button>
         </div>
       </div>
-      <div className={classes.posts}>
-        <Post
-          message={postData[0].message}
-          likeCount={postData[0].likesCount}
-        />
-        <Post
-          message={postData[1].message}
-          likeCount={postData[1].likesCount}
-        />
-      </div>
+      <div className={classes.posts}>{postsElements}</div>
     </div>
   );
 };
