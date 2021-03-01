2fdfff4cf9976ce20e14399837ac950697742e22
smozhevsky <smozhevsky@gmail.com>
2021-03-01 23:36:12 +0300

    add Dialogs and Message component

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,20 +1,7 @@
 import React from "react";
 import classes from "./Dialogs.module.css";
-import { NavLink } from "react-router-dom";
-
-const DialogItem = (props) => {
-  let path = "/dialogs/" + props.id;
-
-  return (
-    <div className={classes.dialog + " " + classes.active}>
-      <NavLink to={path}>{props.name}</NavLink>
-    </div>
-  );
-};
-
-const Message = (props) => {
-  return <div className={classes.message}>{props.message}</div>;
-};
+import DialogItem from "./DialogItem/DialogsItem";
+import Message from "./Message/Message";
 
 const Dialogs = () => {
   let dialogs = [
--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -1,48 +1,8 @@
 import React from "react";
-import classes from "./Dialogs.module.css";
-import { NavLink } from "react-router-dom";
-
-const DialogItem = (props) => {
-  let path = "/dialogs/" + props.id;
-
-  return (
-    <div className={classes.dialog + " " + classes.active}>
-      <NavLink to={path}>{props.name}</NavLink>
-    </div>
-  );
-};
+import classes from "./../Dialogs.module.css";
 
 const Message = (props) => {
   return <div className={classes.message}>{props.message}</div>;
 };
 
-const Dialogs = () => {
-  let dialogs = [
-    { id: 1, name: "Ilya" },
-    { id: 2, name: "Margarita" },
-    { id: 3, name: "Emily" },
-  ];
-
-  let messages = [
-    { id: 1, message: "Hi!" },
-    { id: 2, message: "How are you?" },
-    { id: 3, message: "Yo!" },
-  ];
-
-  let dialogsElements = dialogs.map((d) => (
-    <DialogItem name={d.name} id={d.id} />
-  ));
-
-  let messagesElements = messages.map((m) => {
-    return <Message message={m.message} />;
-  });
-
-  return (
-    <div className={classes.dialogs}>
-      <div className={classes.dialogsItems}>{dialogsElements}</div>
-      <div className={classes.messages}>{messagesElements}</div>
-    </div>
-  );
-};
-
-export default Dialogs;
+export default Message;
