0654b13603db709e80c04496bc494cd71297ec24
smozhevsky <smozhevsky@gmail.com>
2021-05-12 21:31:01 +0300

    add React.createRef() to profile and messages

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -12,10 +12,21 @@ const Dialogs = (props) => {
     return <Message message={m.message} key={index} />;
   });
 
+  let newMessage = React.createRef();
+  let sendMessage = () => {
+    let message = newMessage.current.value;
+    alert(message);
+  };
   return (
     <div className={classes.dialogs}>
       <div className={classes.dialogsItems}>{dialogsElements}</div>
       <div className={classes.messages}>{messagesElements}</div>
+      <div>
+        <textarea ref={newMessage}></textarea>
+      </div>
+      <div>
+        <button onClick={sendMessage}>Send</button>
+      </div>
     </div>
   );
 };
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,15 +3,20 @@ import classes from "./MyPosts.module.css";
 import Post from "./Post/Post.jsx";
 
 const MyPosts = (props) => {
+  let newPostElement = React.createRef();
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  };
   return (
     <div className={classes.postsBlock}>
       <h3>My posts</h3>
       <div>
         <div>
-          <textarea></textarea>
+          <textarea ref={newPostElement}></textarea>
         </div>
         <div>
-          <button>Add new post</button>
+          <button onClick={addPost}>Add new post</button>
         </div>
       </div>
       <div className={classes.posts}>
